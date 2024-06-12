1d864351bb8c7d3cf494252719dc5ae35bf91d13
surifoll <surifoll@yahoo.com>
2024-06-12 09:23:29 +0200

    init

---
+++ src/app/auth-config.ts
@@ -0,0 +1,48 @@
+/**
+ * This file contains authentication parameters. Contents of this file
+ * is roughly the same across other MSAL.js libraries. These parameters
+ * are used to initialize Angular and MSAL Angular configurations in
+ * in app.module.ts file.
+ */
+
+import {
+  LogLevel,
+  Configuration,
+  BrowserCacheLocation,
+} from '@azure/msal-browser';
+
+/**
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ */
+export const msalConfig: Configuration = {
+  auth: {
+    clientId: '6d88f887-d674-4f8c-8485-f183d55b10fb', // This is the ONLY mandatory field that you need to supply.
+    authority: 'https://mamimarketsgmail.ciamlogin.com/', // Replace the placeholder with your tenant subdomain
+    redirectUri: 'http://localhost:4200', // Points to window.location.origin by default. You must register this URI on Azure portal/App Registration.
+    //postLogoutRedirectUri: '/', // Points to window.location.origin by default.
+  },
+  cache: {
+    cacheLocation: BrowserCacheLocation.LocalStorage, // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(logLevel: LogLevel, message: string) {
+        console.log(message);
+      },
+      logLevel: LogLevel.Verbose,
+      piiLoggingEnabled: false,
+    },
+  },
+};
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit:
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+export const loginRequest = {
+  scopes: [],
+};
